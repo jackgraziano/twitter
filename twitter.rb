01d183a75e232b6d5374110ec424af7f31f51cce
Jackson Graziano <jackgraziano@gmail.com>
2017-04-18 09:38:22 -0300

    no puts

--- twitter.rb
+++ twitter.rb
@@ -18,10 +18,10 @@ end
 
 topics = ["lula", "dilma", "temer", "serra", "aecio", "bolsonaro", "bolsomito", "doria", "moro"]
 client.filter(track: topics.join(","), language: 'pt') do |object|
-  puts "*"*30
-  puts "text: " + object.text if object.is_a?(Twitter::Tweet)
-  puts "retweet_count: " + object.retweet_count.to_s if object.is_a?(Twitter::Tweet)
-  puts "source: " + object.source if object.is_a?(Twitter::Tweet)
-  puts "filter_level: " + object.filter_level if object.is_a?(Twitter::Tweet)
-  puts "full_text: " + object.full_text if object.is_a?(Twitter::Tweet)
+  # puts "*"*30
+  # puts "text: " + object.text if object.is_a?(Twitter::Tweet)
+  # puts "retweet_count: " + object.retweet_count.to_s if object.is_a?(Twitter::Tweet)
+  # puts "source: " + object.source if object.is_a?(Twitter::Tweet)
+  # puts "filter_level: " + object.filter_level if object.is_a?(Twitter::Tweet)
+  # puts "full_text: " + object.full_text if object.is_a?(Twitter::Tweet)
 end
